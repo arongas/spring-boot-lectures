09a95150bcb71c832712bbc2fe10493b57b2ad6c
Alex Rongas <a.rongas@gmail.com>
2019-05-20 08:31:35 +0300

    remove lecture2/exersize 1

--- Lecture 2 - Spring Data And Spring Data Rest/exersizes/exersize 1/solution/library/script/dbinit.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS "library";
-DROP USER IF EXISTS library;
-CREATE USER library WITH LOGIN PASSWORD 'library@@@';
-CREATE DATABASE "library" WITH OWNER = library ENCODING = 'UTF8';
--- Lecture 2 - Spring Data And Spring Data Rest/exersizes/exersize 1/solution/library/script/resetDB.bat
+++
@@ -1,1 +0,0 @@
-psql -U postgres -d postgres -a -f dbinit.sql
