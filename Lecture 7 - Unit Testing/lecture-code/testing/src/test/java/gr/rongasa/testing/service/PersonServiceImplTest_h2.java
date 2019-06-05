2c1cd9242d0b48dcd15a7b47eb73b67f2f098474
Alex Rongas <a.rongas@gmail.com>
2019-06-05 10:42:29 +0300

    lecture 7 code quality improvements

--- Lecture 7 - Unit Testing/lecture-code/testing/src/test/java/gr/rongasa/testing/service/PersonServiceImplTest_h2.java
+++ Lecture 7 - Unit Testing/lecture-code/testing/src/test/java/gr/rongasa/testing/service/PersonServiceImplTest_h2.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
